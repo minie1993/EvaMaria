1a0925ada9f5f87662ad05c0a2c3179b92b4289c
minie1993 <93474332+minie1993@users.noreply.github.com>
2021-11-06 08:12:08 +0800

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -706,7 +706,7 @@ async def advantage_spell_chok(msg):
     movielist += [(re.sub(r'(\-|\(|\)|_)', '', i, flags=re.IGNORECASE)).strip() for i in gs_parsed]
     movielist = list(dict.fromkeys(movielist)) # removing duplicates
     if not movielist:
-        k = await msg.reply("I couldn't find anything related to that. Check your spelling")
+        k = await msg.reply("Saya tak dapat cari tajuk anda berikan, lihat typo dan sila cuba lagi")
         await asyncio.sleep(8)
         await k.delete()
         return
@@ -718,7 +718,7 @@ async def advantage_spell_chok(msg):
                 )
             ] for k, movie in enumerate(movielist)]
     btn.append([InlineKeyboardButton(text="Close", callback_data=f'spolling#{user}#close_spellcheck')])
-    await msg.reply("I couldn't find anything related to that\nDid you mean any one of these?", reply_markup=InlineKeyboardMarkup(btn))
+    await msg.reply("Maaf saya tidak jumpa tajuk yang di cari \nCuba lihat senarai tajuk dibawah", reply_markup=InlineKeyboardMarkup(btn))
     
 
 
